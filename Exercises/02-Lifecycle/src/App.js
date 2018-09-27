f756a893bf511a1d689d1840508057ecb49d93d8
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-28 03:59:16 +0530

    Done with 02-Lifecycle

--- Exercises/02-Lifecycle/src/App.js
+++ Exercises/02-Lifecycle/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   /* eslint-disable react/no-unused-state */
   state = {
     lifeCycle: '',
   }
+  componentDidMount() {
+    this.setState({ lifeCycle: 'componentDidMount' });
+  }
+  componentWillReceiveProps() {
+    this.setState({ lifeCycle: 'componentWillReceiveProps' });
+  }
   render() {
     return (
       <div className="App">
-        Welcome to react
+        <p className="lifeCycle">{this.state.lifeCycle || 'Welcome to React'}</p>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  hide: PropTypes.bool,
+};
+
+App.defaultProps = {
+  hide: false,
+};
 
 export default App;
