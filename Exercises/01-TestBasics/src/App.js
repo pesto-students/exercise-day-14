72e898f0fd6241d623f19be2aa68cf77074e619e
vipulrawat <vipulrawat.imi@live.in>
2018-09-28 00:52:00 +0530

    [WIP]-Segment1

--- Exercises/01-TestBasics/src/App.js
+++ Exercises/01-TestBasics/src/App.js
@@ -8,12 +8,26 @@ class App extends Component {
     on: false,
     input: '',
     mainColor: 'blue',
+    pText: 'No!',
+  }
+  handleButtonClick = () => {
+    // e.preventDefault();
+    this.setState({ pText: 'Yes!' });
+  }
+  handleInputChange = (e) => {
+    // e.preventDefault();
+    const text = e.target.value;
+    this.setState({ input: text });
   }
   render() {
     return (
       <div className="App">
         <h1>Welcome to React</h1>
+        <h2>{this.state.input}</h2>
         <div className={this.state.mainColor}>State</div>
+        <p className="button-state">{this.state.pText}</p>
+        <button onClick={this.handleButtonClick}>click</button>
+        <input type="text" onChange={this.handleInputChange} />
       </div>
     );
   }
